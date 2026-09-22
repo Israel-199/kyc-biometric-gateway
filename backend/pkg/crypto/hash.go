4cf701202cecb22ccfa37b11ff3f4610ea1c360d
Israel-199 <israelassefa199@gmail.com>
2026-09-22 09:15:16 -0700

    adding hash package

---
+++ backend/pkg/crypto/hash.go
@@ -0,0 +1,61 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"io"
+)
+
+func HashSHA256(data []byte) string {
+	hash := sha256.Sum256(data)
+	return hex.EncodeToString(hash[:])
+}
+
+func EncryptAES(plainText []byte, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+	return hex.EncodeToString(cipherText), nil
+}
+
+func DecryptAES(cryptoText string, key []byte) ([]byte, error) {
+	cipherText, err := hex.DecodeString(cryptoText)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(cipherText) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, cipherTextBytes := cipherText[:nonceSize], cipherText[nonceSize:]
+	return gcm.Open(nil, nonce, cipherTextBytes, nil)
+}
